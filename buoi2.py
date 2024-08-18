0f2ee916070452ce90c129a8545ea4c524d9cdca
NguyenThiPhuongVan <nvan5780@gmail.com>
2024-08-18 11:03:00 +0700

    Add files via upload

---
+++ buoi2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 07:15:42 2024
+
+@author: nguyenthiphuongvan
+"""
+distance=float(input("Nhập độ dài đoạn đường đến trường(m):"))
+if distance>1200:
+    print("Đường đến trường quá xa.")
+
